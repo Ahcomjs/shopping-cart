e4d72dd0583acecb5c9c11e264bdea9e63c2bdfa
Carlos Meregildo <carlos.delacruz228@gmail.com>
2021-05-13 14:45:57 -0400

    Read course

--- js/app.js
+++ js/app.js
@@ -9,10 +9,20 @@ function loadEventListeners() {
 }
 
 //Functions
-
 function addCourses(event) {
-    event.preventDefault();
+  event.preventDefault();
   if (event.target.classList.contains("add-cart")) {
-    console.log("Adding to cart");
+    const selectedCourse = event.target.parentElement.parentElement;
+    readCourse(selectedCourse);
   }
+}
+
+function readCourse(course) {
+  const courseInfo = {
+    img: course.querySelector("img").src,
+    title: course.querySelector("h4").textContent,
+    price: course.querySelector(".precio span").textContent,
+    id: course.querySelector("a").getAttribute("data-id"),
+    cantidad: 1,
+  };
 }
